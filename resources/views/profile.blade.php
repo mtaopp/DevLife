93f20d6635ba3bf0f36a5bb6012ecfa0f3ad378c
Somrak <Sukritspek1@gmail.com>
2021-09-28 13:45:42 +0200

    Auto stash before merge of "front-end" and "origin/front-end"

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,6 +1,21 @@
+<<<<<<< Updated upstream
 @extends("master")
 
 @section("main")
   <div class="profilewindow">
     
   </div>
+=======
+@extends('master')
+
+@section('pageTitle', 'Profile')
+
+@section('main')
+
+<h2>Welcome [username]</h2>
+
+Username: [usern]
+
+
+@endsection
+>>>>>>> Stashed changes
