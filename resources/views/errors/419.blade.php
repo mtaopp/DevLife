d0d01975a346a1415609364cf0f8fb0ae1559b22
mtaopp <m.ta.opportunity@gmail.com>
2021-10-05 23:21:25 +0200

    419 error page

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <x-slot name="header"></x-slot>
+    <div class="container mt-5 pt-5">
+        <div class="text-center">
+            <h2 class="display-3">419</h2>
+            <p class="display-5">Oops! Something is wrong. <br>
+                419 PAGE EXPIRED</p>
+        </div>
+    </div>
+</x-app-layout>
