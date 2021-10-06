901e3cfc2a228a9d2f612c74874ba9cc6627bcb8
Somrak <Sukritspek1@gmail.com>
2021-10-06 10:57:54 +0200

    registerPageChange

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,9 +1,9 @@
 <div class="container">
-    <div>
+    <div class="register-logo">
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
