570e4bce4e96fc031ea290a4203cb344652b7b92
mtaopp <m.ta.opportunity@gmail.com>
2021-10-07 11:37:41 +0200

    article full

--- resources/views/articleFull.blade.php
+++ resources/views/articleFull.blade.php
@@ -3,22 +3,30 @@
     </x-slot>
 
     <main>
-            
-        <section class="container w-100">
+
+        <section class="container w-100 mt-5">
             <ul>
                 <li class="col-sm-12" id="article">
                     <div class="col-sm-12"  id="entryTitle">
                         <h2 class="articleTitle">{{ $article->title }}</h2>
-                        <span class="cr-by">created by {{ $author[$article->author] }}</span> 
+                        <span class="cr-by">created by {{ $author[$article->author] }}</span>
                         <div class="timestamp">
                             <span class="entryDetails"></span>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-sm-6" id="entryImage">
-                            <img src=" {{ $images[$article->image] }}" alt="" class="entryImage" >
+                            <img src="
+
+                            @if (!is_numeric($article->image))
+                            {{ $article->image }}
+                            @else
+                            {{ $images[$article->image] }}
+                            @endif
+
+                           " alt="" class="entryImage" >
                         </div>
-                    </div>                   
+                    </div>
                     <div class="row">
                         <div class="col-sm-12"  id="entryContent">
                             {{ $article->description}}
@@ -28,6 +36,6 @@
                 </li>
                 <hr>
             </ul>
-        </section>   
+        </section>
     </main>
 </x-app-layout>
