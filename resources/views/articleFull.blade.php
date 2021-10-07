9a110ff8a06cfa24cf04760c8561576c4585f61c
CaroWima <caernwim@hotmail.com>
2021-10-07 11:14:19 +0200

    it works

--- resources/views/articleFull.blade.php
+++ resources/views/articleFull.blade.php
@@ -3,27 +3,6 @@
     </x-slot>
 
     <main>
-            <!-- <div class="container container-full">
-                    <div class="container-main">
-                        <div class="post-container">
-                        <h2 class="post-title">{{$article->title}}</h2>
-                        <span class="created">Article created by {{$article->creator}}</span>
-                        <div class="description">
-                            {{$article->content}}
-                        </div>
-                        <span class="uploaded">{{$article->created_at->diffForHumans()}}</span>
-
-                        <form action="/article/{{$article->id}}" method="post">
-                            @csrf
-
-                            @method('delete')
-                            <button class="new-post button-type" type="submit">Delete post</button>
-
-                            <button class="new-post button-art"><a href="/articles">back to Articles</a></button>
-                        </form>         
-                    </div>
-                </div>
-            </div> -->
         <section class="container w-100">
             <ul>
                 <li class="col-sm-12" id="article">
