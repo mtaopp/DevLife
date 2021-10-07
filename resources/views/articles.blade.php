9c27162d123766bc274a1a3a87ef08ec512e496f
CaroWima <caernwim@hotmail.com>
2021-10-07 08:55:06 +0200

    articles fix

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -15,20 +15,20 @@
                 <ul>
                  @foreach ($articles as $article)
                     <li class="col-sm-12" id="article">
-                        <div class="col-sm-12"  id="articleTitle">
-                             <h2 class="articleTitle"><a href="/article/{{$article->id}}">{{ $article->title }}</a></h2>
+                        <div class="col-sm-12"  id="entryTitle">
+                             <h2 class="entryTitle"><a href="/article/{{$article->id}}">{{ $article->title }}</a></h2>
                             <span class="cr-by">created by {{ $author[$article->author] }}</span>
                         </div>
                         <div class="row">
-                            <div class="col-sm-6" id="articleImage">
+                            <div class="col-sm-6" id="entryImage">
                                 @if (is_numeric($article->image))
-                                    <img src="{{ $images[$article->image] }}" alt="" class="articleImage">
+                                    <img src="{{ $images[$article->image] }}" alt="" class="entryImage">
                                 @else
 
-                                    <img src="{{ $article->image }}" alt="" class="articleImage">
+                                    <img src="{{ $article->image }}" alt="" class="entryImage">
                                 @endif
                             </div>
-                            <div class="col-sm-6"  id="articleContent">
+                            <div class="col-sm-6"  id="entryContent">
                                 {{ $article->description}}
                                 {{\Illuminate\Support\Str::limit($article->content, 150)}}
                                 @if (strlen($article->conent) > 150)
@@ -39,7 +39,7 @@
                             </div>
                         </div>
                         <div class="timestamp">
-                            <span class="articleDetails">{{ $article->created_at->diffForHumans() }}</span>
+                            <span class="entryDetails">{{ $article->created_at->diffForHumans() }}</span>
                         </div>
                     </li>
                     <hr>
